271369086c862316e39d17ccccdab0d070eaf282
Korng Samnang <korngsamnang168@gmail.com>
2024-01-16 22:38:03 +0700

    Fix option for sending cookie

--- server/utils/createSendToken.js
+++ server/utils/createSendToken.js
@@ -14,9 +14,8 @@ export const createSendToken = (user, statusCode, req, res) => {
         //     Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
         // ),
         withCredentials: true,
-        sameSite: "none",
+        sameSite: "None",
         httpOnly: true,
-        domain: "mern-recipe.netlify.app",
         secure: true,
         maxAge: maxAge * 1000,
     });
