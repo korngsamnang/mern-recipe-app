04038f7d733679d400becfb403045282c646106f
Korng Samnang <korngsamnang168@gmail.com>
2024-01-16 16:58:19 +0700

    Change backend base url

--- client/src/services/apiAuth.js
+++ client/src/services/apiAuth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/v1/users",
+    baseURL: "https://v1-recipe-api-92ace21d0c33.herokuapp.com/api/v1/users",
     withCredentials: true,
 });
 
