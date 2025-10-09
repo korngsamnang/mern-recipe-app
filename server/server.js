c1b30c70e3c32e66160d1c48bbe679080bfb7191
Korng Samnang <korngsamnang168@gmail.com>
2025-10-09 15:14:27 +0700

    Update to correct listen port

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ mongoose.connect(db).then(() => {
     console.log("DB connection successful");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`SERVER RUNNING ON PORT: ${PORT}`);
 });
